58be4f780267c3baab11dabd8f29dc4dae252f5e
chenrui2014 <chenruixiang2009@163.com>
2018-06-06 09:58:59 +0800

    api更改

--- src/pages/Page15/Page15.jsx
+++ src/pages/Page15/Page15.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PieDoughnutChart from './components/PieDoughnutChart';
 import RealTimeStatistics from './components/RealTimeStatistics';
-import UserInfoCard from './components/UserInfoCard/UserInfoCard';
 import UserPaintTable from './components/UserPaintTable/UserPaintTable';
 import UserTransactionTable from './components/UserTransactionTable/UserTransactionTable';
 
